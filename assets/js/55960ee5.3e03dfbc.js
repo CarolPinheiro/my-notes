72858911a65c0cc51d952dfcdfa88e6f529f2a99
Carol Pinheiro <caroline@bossabox.com>
2023-01-04 09:55:00 -0300

    Deploy website - based on b52ab2998f0e1b0c9afe8547ee9a55c86b48d41b

---
+++ assets/js/55960ee5.3e03dfbc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdev_docs=self.webpackChunkdev_docs||[]).push([[121],{8070:e=>{e.exports=JSON.parse('[{"label":"writing","permalink":"/my-notes/tags/writing","count":1}]')}}]);
